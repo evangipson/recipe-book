41807ce622231a9d0b64af8e894b20f397d769e8
Evan Gipson <evangipson@gmail.com>
2019-04-25 15:06:06 -0500

    Now working with Tailwind!

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-  "plugins": [
-    {
-      "version": "7.0.14",
-      "plugins": [
-        null,
-        null,
-        null
-      ],
-      "postcssPlugin": "tailwind",
-      "postcssVersion": "7.0.14"
-    },
-    null,
-    null
-  ]
+	"plugins": [
+ 		require('tailwindcss')('tailwind.js'),
+ 		require('autoprefixer')(),
+	]
 }
